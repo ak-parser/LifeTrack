cd6e4b170f5e327fed9e85dff2c44ab5573e0ca2
Олег Федак <oleh.fedak.pz.2020@lpnu.ua>
2023-12-04 01:47:36 +0200

    Create new files

---
+++ lifetrack.client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css';
+
+const Home = () => {
+    return (
+      <>
+        <div className="main">
+            <div className="layout">
+                <p>Hello world</p>
+            </div>
+        </div>
+      </>
+    )
+  };
+  
+  export default Home;
---
+++ lifetrack.client/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import './Login.css';
+
+const Login = () => {
+    return (
+      <>
+        <div className="main">
+            <div className="layout">
+                <p>Hello world</p>
+            </div>
+        </div>
+      </>
+    )
+  };
+  
+  export default Login;
